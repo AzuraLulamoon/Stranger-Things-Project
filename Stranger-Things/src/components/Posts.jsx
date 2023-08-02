ddac6d9bcffb9f51dc1c94ab344cd342d5fa198e
AzuraLulamoon <133188559+AzuraLulamoon@users.noreply.github.com>
2023-08-02 11:45:46 -0500

    All posts are displaying properly now, fixed bug with profile page

--- Stranger-Things/src/components/Posts.jsx
+++ Stranger-Things/src/components/Posts.jsx
@@ -1,30 +1,31 @@
 // import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
-import { Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const COHORT_NAME = '2209-FTB-ET-WEB-FT'
 const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`
 
 export default function Posts() {
     const [ posts, setPosts ] = useState([])
-    const [errorMessage, setErrorMessage] = useState(''); 
+    const [errorMessage, setErrorMessage] = useState('');
+    const navigate = useNavigate();
 
     useEffect(() => {
     
         async function fetchData() {
             try {
                 const response = await fetch(`${BASE_URL}/posts`)
-                const data = response.json();
+                const data = await response.json();
 
-                console.log(data);
+                console.log(data.data.posts);
                 setPosts(data.data.posts);
             } catch(error) {
                 setErrorMessage(error);
             }
         }
             
-            fetchData();
+        fetchData();
     }, [])
     
     return <>
@@ -33,10 +34,11 @@ export default function Posts() {
         </div>
         {posts.map((posts, index) => 
             <div key={index} className="allPosts"
-                onClick={() => Navigate(`/$posts.id`)}>
+                onClick={() => navigate(`{/$posts.id}`)}>
                 <h1>{posts.title}</h1>
-                <h3>{posts.location}</h3>
-                <h3>{posts.price}</h3>
+                <h3>Location: {posts.location}</h3>
+                <h3>Price: {posts.price}</h3>
+                <p>{posts.description}</p>
             </div>
         )} 
     </>  
