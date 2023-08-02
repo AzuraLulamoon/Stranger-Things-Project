66cfdff14e3fa28c35d1b7c1ebcda8066947134d
AzuraLulamoon <133188559+AzuraLulamoon@users.noreply.github.com>
2023-08-02 10:40:28 -0500

    All basic page components created, plus some minor additions minus all logic. logic implementation is next

---
+++ Stranger-Things/src/components/NewAccount.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+
+function CreateAccount() {
+  // State to hold the input values
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  // Function to handle form submission
+  const handleSubmit = (event) => {
+    event.preventDefault();
+        //backend Logic
+    console.log('Username:', username);
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
+
+  return (
+    <div>
+      <h1>Create a New Account</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Create Account</button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateAccount;
