d2debbdc747dd9ba138f7e5b5d284dbb440386b4
AzuraLulamoon <133188559+AzuraLulamoon@users.noreply.github.com>
2023-08-05 20:09:40 -0500

    lots of commit stuff, totally forgot to commit and codded for hours xD

--- Stranger-Things/src/components/Profile.jsx
+++ Stranger-Things/src/components/Profile.jsx
@@ -1,34 +1,50 @@
-import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+const COHORT_NAME = '2209-FTB-ET-WEB-FT'
+const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`
 
 export default function Profile() {
 
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [user, setUser] = useState('');
+    const token = localStorage.getItem('token');
+    const navigate = useNavigate();
 
     const handleLogout = () => {
-        //logic here
-        setIsLoggedIn(false);
+        
+        setUser(null);
+        localStorage.removeItem('token');
+        navigate('/Login');
     }
+    
+    useEffect(() => {
+    if (token) {
+        fetch(`${BASE_URL}/users/me`, {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        })
+        const response = 
+        .then((userData) => {
+            setUser(userData);
+        })
+        .catch((error) => {
+            console.error(error);
+        })
+      }
+    
+    }, []);
 
-    if (!isLoggedIn) {
-
-        return (
-            <div>
-                <h1>My Profile</h1>
-                <p><Link to='/Login'>You must login first!</Link></p>
-                <p><Link to='/NewAccount'>Dont have an account? Create one here!</Link></p>
-            </div>
-        )
-    }
-
-    return (
+    console.log(user)
+    return <>
         <div>
-            <h1>Your Profile!</h1>
-            <p>Profile Information</p>
-            {/* Profile Info */}
-            {/* Your Posts */}
-            {/* Your messages */}
-            <button onClick={handleLogout}>Logout</button>
+            
         </div>
-    )
+            <div>
+            <Link to='/NewPost'>Want to Post something?</Link>
+            <br></br>
+            <button onClick={handleLogout}>Logout</button>        
+       </div>
+    </>
 }
